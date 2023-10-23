8f56343a72763d8835f6d28e9d303632f77189c9
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2023-10-23 23:40:04 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+        double a = Integer.parseInt(st.nextToken());
+        double b = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(a / b));
+        bw.flush();
+        bw.close();
+    }
+}
