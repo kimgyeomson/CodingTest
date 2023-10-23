af65577b449a68568bbef691bbbffe969254af6e
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2023-10-23 23:44:03 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -5,22 +5,17 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String s = br.readLine();
+        
         StringTokenizer st = new StringTokenizer(s);
+        
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-
-        bw.write(String.valueOf(a + b));
-        bw.newLine();
-        bw.write(String.valueOf(a - b));
-        bw.newLine();
-        bw.write(String.valueOf(a * b));
-        bw.newLine();
-        bw.write(String.valueOf(a / b));
-        bw.newLine();
-        bw.write(String.valueOf(a % b));
-        bw.flush();
-        bw.close();
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
     }
 }
