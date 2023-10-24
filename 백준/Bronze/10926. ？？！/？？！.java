961fa9a7f62b24383b5df38760bd40f8235dd73c
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2023-10-25 00:18:45 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(br.readLine());
+        sb.append("??!");
+        System.out.println(sb.toString());
+    }
+}
