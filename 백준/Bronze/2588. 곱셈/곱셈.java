2b69edf2f32735fe1a81e93f402ede8c873049e2
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2023-10-27 00:39:20 +0900

    [Bronze III] Title: 곱셈, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        in.close();
+        System.out.println(a*(b%10));
+        System.out.println(a*(b%100/10));
+        System.out.println(a*(b/100));
+        System.out.println(a*b);
+    }
+}
