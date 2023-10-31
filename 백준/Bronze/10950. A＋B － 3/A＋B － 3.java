f42e133beea5fcf9a326bebaa06e11f86e7b5f36
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2023-10-31 22:17:57 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -4,19 +4,14 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[][] result = new int[n][2];
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i ++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            result[i][0] = Integer.parseInt(st.nextToken());
-            result[i][1] = Integer.parseInt(st.nextToken());
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+            sb.append("\n");
         }
-
-        for(int i = 0; i < n; i++) {
-            int sum = 0;
-            sum = result[i][0] + result[i][1];
-            System.out.println(sum);
-        }
+        System.out.println(sb);
     }
 }
