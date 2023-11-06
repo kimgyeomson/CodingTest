325faa5aa66be3e97ce139707603c6b56709ce67
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2023-11-06 14:59:36 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String a = br.readLine();
-        br.close();
         System.out.println(a.length());
     }
 }
