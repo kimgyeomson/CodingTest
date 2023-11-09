c8ff05bb27bee6150a2ce026f28c8eee33ddb4be
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2023-11-09 22:20:04 +0900

    [Bronze II] Title: 진법 변환, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String s = st.nextToken();
+        int n = Integer.parseInt(st.nextToken());
+        System.out.println(Integer.parseInt(s, n));
+    }
+}
